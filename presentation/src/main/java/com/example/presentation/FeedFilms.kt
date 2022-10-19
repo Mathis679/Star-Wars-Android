54abcd3e09e2e365e2048842ab1351c46769ac37
mathis <m.laurent@heolys.fr>
2022-10-19 22:20:52 +0200

    add custom theme + display image while data not loaded

--- presentation/src/main/java/com/example/presentation/FeedFilms.kt
+++ presentation/src/main/java/com/example/presentation/FeedFilms.kt
@@ -1,16 +1,28 @@
 package com.example.presentation
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import com.example.domain.model.Film
 
 @Composable
 fun FeedFilms(films: List<Film>) {
-    Surface(Modifier.fillMaxSize()) {
+    Surface(
+        color = MaterialTheme.colors.primary,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        if(films.isEmpty()){
+            LoadingView()
+        }
         LazyColumn{
             items(
                 films.size,
@@ -26,5 +38,17 @@ fun FeedFilms(films: List<Film>) {
 
 @Composable
 fun FilmItem(film: Film){
-    Text(text = film.title)
+    Text(
+        text = film.title
+    )
+}
+
+@Composable
+fun LoadingView(){
+    Image(
+        painterResource(R.drawable.ic_stormtrooper),
+        contentDescription = "stormtrooper loading",
+        contentScale = ContentScale.Crop,
+        modifier = Modifier.size(50.dp)
+    )
 }
--- presentation/src/main/java/com/example/presentation/MainActivity.kt
+++ presentation/src/main/java/com/example/presentation/MainActivity.kt
@@ -5,12 +5,16 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import com.example.domain.model.Film
 import com.example.utils.constants.FEED_FILMS_SCOPE_ID
 import com.example.utils.constants.FEED_FILMS_SCOPE_NAME
@@ -28,11 +32,21 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             val films by feedFilmViewModel?.films?.collectAsState() as State<List<Film>>
-            FeedFilms(films = films)
+            AppTheme {
+                FeedFilms(films = films)
+            }
         }
 
 
         bindScope(feedFilmViewModelScope)
 
+    }
+
+    @Composable
+    fun AppTheme(content: @Composable () -> Unit){
+        val colors = lightColors(
+            primary = Color(0xff272727)
+        )
+        MaterialTheme(colors = colors, content = content)
     }
 }
